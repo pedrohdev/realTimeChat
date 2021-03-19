5b4d6a3a34817d174afbaf1404478ad7c865ccd8
Pedro Henrique Vieira <64736691+PedroHdev@users.noreply.github.com>
2021-03-19 13:22:34 -0300

    Chat

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+
+const app = express()
+const port = 3000
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static('public'))
+
+app.engine('handlebars', handlebars({
+    defaultLayout: 'main',
+    layoutsDir: './views/layouts',
+    partialsDir: './views/partials'
+}))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    res.render('chat')
+})
+
+let messages = []
+
+io.on('connection', socket => {
+    socket.on('send', data => {
+        messages.push(data)
+        io.emit('received', data)
+    })
+})
+
+server.listen(port)
